9d070fce8962dcab6223c7ad928243a73014ea3d
Pablo Romero Montero <pabloromeromontero23@gmail.com>
2020-02-16 14:54:51 +0100

    Install storage, antes del modal

--- src/app/pages/articulos-detalle/articulos-detalle.page.ts
+++ src/app/pages/articulos-detalle/articulos-detalle.page.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Articulos} from '../../interfaces/articulos';
+import {FirebaseService} from '../../services/firebase.service';
 
 @Component({
-  selector: 'app-articulos-detalle',
-  templateUrl: './articulos-detalle.page.html',
-  styleUrls: ['./articulos-detalle.page.scss'],
+    selector: 'app-articulos-detalle',
+    templateUrl: './articulos-detalle.page.html',
+    styleUrls: ['./articulos-detalle.page.scss'],
 })
 export class ArticulosDetallePage implements OnInit {
+    params: any;
+    articulo: Articulos;
 
-  constructor() { }
+    constructor(private router: ActivatedRoute, private fire: FirebaseService) {
+        this.router.params.subscribe(params => {
+            this.params = params.id;
 
-  ngOnInit() {
-  }
+        });
+
+
+        this.fire.getArticulos().subscribe(data => {
+            for (const articulo of data) {
+                if (this.params === articulo.id) {
+                    this.articulo = articulo;
+                }
+            }
+        });
+    }
+
+    ngOnInit() {
+        //     this.router.params.subscribe(params => {
+        //         this.fire.getArticulos().subscribe(data => {
+        //             this.params = params.id;
+        //             for (const articulo of data) {
+        //                 if (this.params === articulo.id) {
+        //                     this.articulo = articulo;
+        //                 }
+        //             }
+        //         });
+        //     });
+    }
 
 }
